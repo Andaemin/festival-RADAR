e3a9c42763260334aaf402031202aef53422861a
andaemin <eoals0216@gmail.com>
2026-08-05 18:32:24 +0900

    first Commit

---
+++ backend/src/main/java/com/festivalradar/FestivalRadarApplication.java
@@ -0,0 +1,13 @@
+package com.festivalradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalRadarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/festivalradar/FestivalRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festivalradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
